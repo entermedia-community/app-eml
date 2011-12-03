17e8eebe19a595ef5f78c14de2b4672b58100c1d
cburkey <cburkey@entermediasoftware.com>
2011-12-03 16:17:51 -0500

    Saved search

--- webapp/eml/components/javascript/eml.js
+++ webapp/eml/components/javascript/eml.js
@@ -24,5 +24,62 @@ $(document).ready(function() {
 			}
 		);
 	
+	$("#savedquerylist a").click(function(e)
+					{
+						e.preventDefault();
+						var a = jQuery(this);
+						var link = a.attr("href");
+						
+						jQuery.get(link, {}, function(data) 
+								{
+									var toreplace = jQuery("#searcheditor");
+									toreplace.html(data);
+									
+									var tmp = jQuery("#savedquerylist #newterm");
+									tmp.remove();
+									var top = a.position().top;
+									top = top + a.height() + 40;
+									jQuery("#eml-green-dialog").css("top",top);
+									
+									jQuery("#arrow").show();
+									var padleft = a.position().left;
+									padleft = padleft + a.width() / 2;
+									padleft = padleft  - 42; //arrow width
+									jQuery("#arrow").css("left",padleft);
+								}
+						);
+						return false;
+					}
+			);
+	
+	$("#addterm").click(function(e)
+			{
+				e.preventDefault();
+				var a = jQuery(this);
+				var link = a.attr("href");
+				
+				jQuery.get(link, {}, function(data) 
+						{
+							var toreplace = jQuery("#searcheditor");
+							toreplace.html(data);
+							
+							jQuery("#savedquerylist span").append('<span id="newterm">new term</span>');
+							var a = jQuery("#savedquerylist #newterm");
+							var top = a.position().top;
+							top = top + a.height() + 40;
+							jQuery("#eml-green-dialog").css("top",top);
+							
+							jQuery("#arrow").show();
+							var padleft = a.position().left;
+							padleft = padleft + a.width() / 2;
+							padleft = padleft  - 42; //arrow width
+							jQuery("#arrow").css("left",padleft);
+						}
+				);
+				return false;
+			}
+	);
+
+	
 
 });
