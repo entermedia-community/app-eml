292aaffd7875c7d4cd73ade3997cf74f243bcf90
rob <rob@openedit.org>
2012-02-13 11:48:38 -0500

    updates to theme styles and javascript

--- webapp/eml/components/javascript/eml.js
+++ webapp/eml/components/javascript/eml.js
@@ -128,36 +128,35 @@ emlcomponents = function()
 			}
 		}
 	);
-
-$("#savedquerylist a").click(function(e)
-				{
-					e.preventDefault();
-					var a = jQuery(this);
-					var link = a.attr("href");
-					
-					jQuery.get(link, {}, function(data) 
-							{
-								var toreplace = jQuery("#searcheditor");
-								toreplace.html(data);
-								
-								var tmp = jQuery("#savedquerylist #newterm");
-								tmp.remove();
-								var top = a.position().top;
-								top = top + a.height() + 40;
-								jQuery("#eml-green-dialog").css("top",top);
-								
-								jQuery("#arrow").show();
-								var padleft = a.position().left;
-								padleft = padleft + a.width() / 2;
-								padleft = padleft  - 42; //arrow width
-								jQuery("#arrow").css("left",padleft);
-							}
-					);
-					return false;
-				}
-		);
-
-$("#addterm").click(function(e)
+	$("#savedquerylist a").click(function(e)
+			{
+				e.preventDefault();
+				var a = jQuery(this);
+				var link = a.attr("href");
+				
+				jQuery.get(link, {}, function(data) 
+						{
+							var toreplace = jQuery("#searcheditor");
+							toreplace.html(data);
+							
+							var tmp = jQuery("#savedquerylist #newterm");
+							tmp.remove();
+							var top = a.position().top;
+							top = top + a.height() + 40;
+							jQuery("#eml-green-dialog").css("top",top);
+							
+							jQuery("#arrow").show();
+							var padleft = a.position().left;
+							padleft = padleft + a.width() / 2;
+							padleft = padleft  - 42; //arrow width
+							jQuery("#arrow").css("left",padleft);
+						}
+				);
+				return false;
+			}
+	);
+	
+	$("#addterm").click(function(e)
 		{
 			e.preventDefault();
 			var a = jQuery(this);
@@ -183,9 +182,9 @@ $("#addterm").click(function(e)
 			);
 			return false;
 		}
-);
-
+	);
 }
+
 
 function setCookie(name,value,days) {
     if (days) {
